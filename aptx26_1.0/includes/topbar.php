9673ba547dcffbfd0da2b29b5c104406ef839b4d
Joe Troppmair <joe@troppmair.com>
2026-09-14 12:52:48 +0200

    Daten löschen

--- aptx26_1.0/includes/topbar.php
+++
@@ -1,8 +0,0 @@
-<?php defined('_JEXEC') or die; ?>
-
-<?php if ( $this->countModules('startslider') ) : ?>
-	<div id="jteSlider"><jdoc:include type="modules" name="startslider" style="html5" /></div>
-	
-<?php else: ?>
-	<div id="jteFlex"><jdoc:include type="modules" name="flexheadertop" style="html5" /></div>
-<?php endif; ?>
--- aptx26_1.6/includes/wa-chat.php
+++
@@ -1,5 +0,0 @@
-<!-- Elfsight WhatsApp Chat | ApTx Chat -->
-<template consenty-if="elfsight">
-<script src="https://static.elfsight.com/platform/platform.js" async></script>
-<div class="elfsight-app-dd375941-19a9-4c6e-a925-c2f58f377783" data-elfsight-app-lazy></div>
-</template>
